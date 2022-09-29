cee7120d7d78ccbca481ded2d213b3d2eb9eb7db
Orhorhaghe Ejiro Benjamin <heyjis4connection@gmail.com>
2022-09-28 18:57:54 -0700

    Write a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,43 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns value of x raised to power of y
- * @x: integer base
- * @y: integer exponent
- * Return: integer power
+ * power_operation - returns the natural square root of a number.
+ *
+ * @n: input number.
+ *
+ * @c: iterator.
+ *
+ * Return: square root or -1.
  */
 
-int _pow_recursion(int x, int y)
+int power_operation(int n, int c)
 {
-	if (y > 0)
+	if (c % (n / c) == 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
 	}
-	else if (y == 0)
-	{
-		return (1);
-	}
-	else
+	return (0 + power_operation(n, c + 1));
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ *
+ * @n: input number.
+ *
+ * Return: natural square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
 		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
